b1cb621d5b52dc307abc69fe126aabf74ac06c10
pscarlett027 <114139469+pscarlett027@users.noreply.github.com>
2022-09-26 16:14:04 +0100

    Update app_start.sh
    add check to see if node is running, if it is then exit.

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 cd /home/ec2-user/server/
+#!/bin/bash
+SERVICE="node"
+if pgrep -x "$SERVICE" >/dev/null
+then
+   exit
+else
+    npm start
+    # uncomment to start nginx if stopped
+    # systemctl start nginx
+    # mail  
+fi
+exit 
+
+# original code is below
+
 #ps aux  |  grep -i node  |  awk '{print $2}'  |  xargs sudo kill -9
 #npm restart & 
 #pm2 start npm --name "simple-reactjs-app" -- start
