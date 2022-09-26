1dfc3adf5ad1ea06923de96f991b2a51d385bb32
pscarlett027 <114139469+pscarlett027@users.noreply.github.com>
2022-09-26 15:29:34 +0100

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/server/
 #ps aux  |  grep -i node  |  awk '{print $2}'  |  xargs sudo kill -9
-npm start 
-#npm run build
+#npm start 
+npm run build
 #pm2 start npm --name "simple-reactjs-app" -- start
 #pm2 startup
 #pm2 save
