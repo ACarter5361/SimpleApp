1672b90e427dc99ed283bd83fea187cb9ae342d6
pscarlett027 <114139469+pscarlett027@users.noreply.github.com>
2022-09-27 11:10:47 +0100

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -6,7 +6,7 @@ if pgrep -x "$SERVICE" >/dev/null
 then
    exit
 else
-    npm start& echo "Services started"
+    npm run build
     # uncomment to start nginx if stopped
     # systemctl start nginx
     # mail  
