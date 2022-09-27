bd3e6e88d7c1d3f02156adefcf26b5a849ddfd9e
pscarlett027 <114139469+pscarlett027@users.noreply.github.com>
2022-09-27 11:46:53 +0100

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo yum -y update
+sudo yum install httpd.x86_64
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
 cd /home/ec2-user/server
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo yum -y install nodejs npm
